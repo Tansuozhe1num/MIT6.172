be8cfdbfbfb7d1459548c17f596257a6998245d6
Dream_startooo <511017802@qq.com>
2026-04-26 09:34:10 +0800

    change sort_c using faster sort algorithm

--- homework2 - perf/homework/sort_c.c
+++ homework2 - perf/homework/sort_c.c
@@ -20,10 +20,91 @@
  * IN THE SOFTWARE.
  **/
 
-
 #include "./util.h"
 
+#define INSERTION_SORT_THRESHOLD 16
+
+// Function prototypes
+void isort(data_t* begin, data_t* end);
+static inline void merge_c(data_t* A, int p, int q, int r);
+static inline void copy_c(const data_t* source, data_t* dest, int n);
+
+// A basic merge sort routine that sorts the subarray A[p..r]
 void sort_c(data_t* A, int p, int r) {
-  printf("Unimplemented!\n");
+  assert(A);
+
+  if (p >= r) {
+    return;
+  }
+
+  int n = r - p + 1;
+  if (n <= INSERTION_SORT_THRESHOLD) {
+    isort(A + p, A + r);
+    return;
+  }
+
+  int q = (p + r) / 2;
+  sort_c(A, p, q);
+  sort_c(A, q + 1, r);
+  merge_c(A, p, q, r);
 }
 
+// A merge routine. Merges the sub-arrays A[p..q] and A[q + 1..r].
+// Uses two arrays 'left' and 'right' in the merge operation.
+static inline void merge_c(data_t* A, int p, int q, int r) {
+  assert(A);
+  assert(p <= q);
+  assert((q + 1) <= r);
+
+  int n1 = q - p + 1;
+  int n2 = r - q;
+
+  data_t* left = NULL;
+  data_t* right = NULL;
+
+  mem_alloc(&left, n1 + 1);
+  mem_alloc(&right, n2 + 1);
+
+  if (left == NULL || right == NULL) {
+    mem_free(&left);
+    mem_free(&right);
+    return;
+  }
+
+  copy_c(A + p, left, n1);
+  copy_c(A + q + 1, right, n2);
+
+  left[n1] = UINT_MAX;
+  right[n2] = UINT_MAX;
+
+  data_t* lcur = left;
+  data_t* rcur = right;
+  data_t* acur = A + p;
+
+  for (; acur <= A + r; ++acur) {
+    if (*lcur <= *rcur) {
+      *acur = *lcur;
+      ++lcur;
+    } else {
+      *acur = *rcur;
+      ++rcur;
+    }
+  }
+
+  mem_free(&left);
+  mem_free(&right);
+}
+
+static inline void copy_c(const data_t* source, data_t* dest, int n) {
+  assert(dest);
+  assert(source);
+
+  const data_t* s = source;
+  data_t* d = dest;
+
+  for (int i = 0; i < n; ++i) {
+    *d = *s;
+    ++d;
+    ++s;
+  }
+}
