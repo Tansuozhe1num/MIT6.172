e6fb01a2edf7f8d7eec03d053ef082a3d71b0f61
Dream_startooo <511017802@qq.com>
2026-04-26 09:26:58 +0800

    change sort_p as pointer sort version

--- homework2 - perf/homework/sort_p.c
+++ homework2 - perf/homework/sort_p.c
@@ -20,10 +20,80 @@
  * IN THE SOFTWARE.
  **/
 
-
 #include "./util.h"
 
+// Function prototypes
+static inline void merge_p(data_t* A, int p, int q, int r);
+static inline void copy_p(const data_t* source, data_t* dest, int n);
+
+// A basic merge sort routine that sorts the subarray A[p..r]
 void sort_p(data_t* A, int p, int r) {
-  printf("Unimplemented!\n");
+  assert(A);
+  if (p < r) {
+    int q = (p + r) / 2;
+    sort_p(A, p, q);
+    sort_p(A, q + 1, r);
+    merge_p(A, p, q, r);
+  }
 }
 
+// A merge routine. Merges the sub-arrays A[p..q] and A[q + 1..r].
+// Uses two arrays 'left' and 'right' in the merge operation.
+static inline void merge_p(data_t* A, int p, int q, int r) {
+  assert(A);
+  assert(p <= q);
+  assert((q + 1) <= r);
+
+  int n1 = q - p + 1;
+  int n2 = r - q;
+
+  data_t* left = NULL;
+  data_t* right = NULL;
+
+  mem_alloc(&left, n1 + 1);
+  mem_alloc(&right, n2 + 1);
+
+  if (left == NULL || right == NULL) {
+    mem_free(&left);
+    mem_free(&right);
+    return;
+  }
+
+  // 用指针代替数组下标访问
+  copy_p(A + p, left, n1);
+  copy_p(A + q + 1, right, n2);
+
+  left[n1] = UINT_MAX;
+  right[n2] = UINT_MAX;
+
+  data_t* lcur = left;
+  data_t* rcur = right;
+  data_t* acur = A + p;
+
+  for (; acur <= A + r; ++acur) {
+    if (*lcur <= *rcur) {
+      *acur = *lcur;
+      ++lcur;
+    } else {
+      *acur = *rcur;
+      ++rcur;
+    }
+  }
+
+  mem_free(&left);
+  mem_free(&right);
+}
+
+static inline void copy_p(const data_t* source, data_t* dest, int n) {
+  assert(dest);
+  assert(source);
+
+  const data_t* s = source;
+  data_t* d = dest;
+
+  for (int i = 0; i < n; ++i) {
+    *d = *s;
+    ++d;
+    ++s;
+  }
+}
