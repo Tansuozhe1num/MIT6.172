9b355d530182db4ff6664616ecad65ab49f8f9b1
Dream_startooo <511017802@qq.com>
2026-04-26 09:48:37 +0800

    change sort_f: reuse memory

--- homework2 - perf/homework/sort_f.c
+++ homework2 - perf/homework/sort_f.c
@@ -1,29 +1,80 @@
-/**
- * Copyright (c) 2012 MIT License by 6.172 Staff
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to
- * deal in the Software without restriction, including without limitation the
- * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
- * sell copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
- * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
- * IN THE SOFTWARE.
- **/
-
-
 #include "./util.h"
 
+#define INSERTION_SORT_THRESHOLD 16
+
+void isort(data_t* begin, data_t* end);
+static inline void copy_f(const data_t* source, data_t* dest, int n);
+static inline void merge_f(data_t* A, int p, int q, int r, data_t* temp);
+static void sort_f_rec(data_t* A, int p, int r, data_t* temp);
+
+// 对外接口：只分配一次
 void sort_f(data_t* A, int p, int r) {
-  printf("Unimplemented!\n");
+  assert(A);
+  if (p >= r) return;
+
+  int n = r - p + 1;
+
+  // 临时缓冲区大小至少要能容纳最大左半边
+  data_t* temp = NULL;
+  mem_alloc(&temp, n / 2 + 1);
+  if (temp == NULL) {
+    return;
+  }
+
+  sort_f_rec(A, p, r, temp);
+
+  mem_free(&temp);
 }
 
+// 递归辅助函数
+static void sort_f_rec(data_t* A, int p, int r, data_t* temp) {
+  assert(A);
+  assert(temp);
+
+  int n = r - p + 1;
+  if (n <= INSERTION_SORT_THRESHOLD) {
+    isort(A + p, A + r);
+    return;
+  }
+
+  int q = (p + r) / 2;
+  sort_f_rec(A, p, q, temp);
+  sort_f_rec(A, q + 1, r, temp);
+  merge_f(A, p, q, r, temp);
+}
+
+// 只拷贝左半边到 temp，右半边留在 A 里
+static inline void merge_f(data_t* A, int p, int q, int r, data_t* temp) {
+  assert(A);
+  assert(temp);
+  assert(p <= q);
+  assert(q + 1 <= r);
+
+  int n1 = q - p + 1;
+  copy_f(A + p, temp, n1);
+
+  int i = n1 - 1;   // temp 指针末尾
+  int j = r;        // A 中右半边末尾
+  int k = r;        // 写回位置
+
+  while (i >= 0 && j >= q + 1) {
+    if (temp[i] > A[j]) {
+      A[k--] = temp[i--];
+    } else {
+      A[k--] = A[j--];
+    }
+  }
+
+  while (i >= 0) {
+    A[k--] = temp[i--];
+  }
+}
+
+static inline void copy_f(const data_t* source, data_t* dest, int n) {
+  assert(dest);
+  assert(source);
+
+  for (int i = 0; i < n; ++i) {
+    dest[i] = source[i];
+  }
+}
