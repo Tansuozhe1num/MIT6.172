803c0817799aea580b1546d0cd40f77907550bd4
Dream_startooo <511017802@qq.com>
2026-04-26 16:30:40 +0800

    optimize 2

--- homework4 - cilk/queens.c
+++ homework4 - cilk/queens.c
@@ -2,10 +2,11 @@
 #include <stdlib.h>
 #include <stdint.h>
 #include <cilk/cilk.h>
+#include <cilk/reducer.h>
 
 #define N 8
 #define ALL ((1u << N) - 1u)
-#define CUTOFF 4   // 可按需要调整，越大越偏串行，越小 spawn 越多
+#define CUTOFF 4   // 可按需要调整
 
 typedef struct Node {
     uint64_t sol;
@@ -17,12 +18,6 @@ typedef struct {
     Node* tail;
     int size;
 } BoardList;
-
-static void init_list(BoardList* list) {
-    list->head = NULL;
-    list->tail = NULL;
-    list->size = 0;
-}
 
 static void append_solution(BoardList* list, uint64_t sol) {
     Node* node = (Node*)malloc(sizeof(Node));
@@ -42,33 +37,65 @@ static void append_solution(BoardList* list, uint64_t sol) {
     list->size++;
 }
 
-static void merge_lists(BoardList* list1, BoardList* list2) {
-    if (list2->head == NULL) {
-        init_list(list2);
-        return;
-    }
-
-    if (list1->head == NULL) {
-        *list1 = *list2;
-        init_list(list2);
-        return;
-    }
-
-    list1->tail->next = list2->head;
-    list1->tail = list2->tail;
-    list1->size += list2->size;
-    init_list(list2);
+static void board_list_identity(void* key, void* value) {
+    (void)key;
+    BoardList* list = (BoardList*)value;
+    list->head = NULL;
+    list->tail = NULL;
+    list->size = 0;
 }
 
-static void free_list(BoardList* list) {
+static void board_list_reduce(void* key, void* left, void* right) {
+    (void)key;
+    BoardList* L = (BoardList*)left;
+    BoardList* R = (BoardList*)right;
+
+    if (R->head == NULL) {
+        return;
+    }
+
+    if (L->head == NULL) {
+        *L = *R;
+        R->head = NULL;
+        R->tail = NULL;
+        R->size = 0;
+        return;
+    }
+
+    L->tail->next = R->head;
+    L->tail = R->tail;
+    L->size += R->size;
+
+    R->head = NULL;
+    R->tail = NULL;
+    R->size = 0;
+}
+
+static void board_list_destroy(void* key, void* value) {
+    (void)key;
+    BoardList* list = (BoardList*)value;
+
     Node* cur = list->head;
     while (cur != NULL) {
         Node* nxt = cur->next;
         free(cur);
         cur = nxt;
     }
-    init_list(list);
+
+    list->head = NULL;
+    list->tail = NULL;
+    list->size = 0;
 }
+
+typedef CILK_C_DECLARE_REDUCER(BoardList) BoardListReducer;
+
+static BoardListReducer X = CILK_C_INIT_REDUCER(
+    BoardList,
+    board_list_reduce,
+    board_list_identity,
+    board_list_destroy,
+    (BoardList){ .head = NULL, .tail = NULL, .size = 0 }
+);
 
 static void print_solution(uint64_t sol) {
     for (int r = 0; r < N; ++r) {
@@ -82,9 +109,9 @@ static void print_solution(uint64_t sol) {
 }
 
 static void try_serial(unsigned int row, unsigned int left, unsigned int right,
-                       int depth, uint64_t sol, BoardList* out) {
+                       int depth, uint64_t sol) {
     if (row == ALL) {
-        append_solution(out, sol);
+        append_solution(&REDUCER_VIEW(X), sol);
         return;
     }
 
@@ -97,61 +124,50 @@ static void try_serial(unsigned int row, unsigned int left, unsigned int right,
                    (left | place) << 1,
                    (right | place) >> 1,
                    depth + 1,
-                   sol | ((uint64_t)place << (depth * 8)),
-                   out);
+                   sol | ((uint64_t)place << (depth * 8)));
     }
 }
 
 static void try(unsigned int row, unsigned int left, unsigned int right,
-                int depth, uint64_t sol, BoardList* out) {
+                int depth, uint64_t sol) {
     if (row == ALL) {
-        append_solution(out, sol);
+        append_solution(&REDUCER_VIEW(X), sol);
         return;
     }
 
     if (depth >= CUTOFF) {
-        try_serial(row, left, right, depth, sol, out);
+        try_serial(row, left, right, depth, sol);
         return;
     }
 
     unsigned int poss = ~(row | left | right) & ALL;
-    BoardList child_lists[N];
-    int child_cnt = 0;
 
     while (poss != 0) {
         unsigned int place = poss & -poss;
         poss &= ~place;
 
-        int idx = child_cnt++;
-        init_list(&child_lists[idx]);
-
         cilk_spawn try(row | place,
                        (left | place) << 1,
                        (right | place) >> 1,
                        depth + 1,
-                       sol | ((uint64_t)place << (depth * 8)),
-                       &child_lists[idx]);
+                       sol | ((uint64_t)place << (depth * 8)));
     }
 
     cilk_sync;
-
-    for (int i = 0; i < child_cnt; ++i) {
-        merge_lists(out, &child_lists[i]);
-    }
 }
 
 int main(void) {
-    BoardList ans;
-    init_list(&ans);
+    CILK_C_REGISTER_REDUCER(X);
 
-    try(0, 0, 0, 0, 0, &ans);
+    try(0, 0, 0, 0, 0);
 
-    printf("There are %d solutions.\n", ans.size);
+    printf("There are %d solutions.\n", X.value.size);
 
-    // for (Node* p = ans.head; p != NULL; p = p->next) {
+    // 如果你想打印所有解，取消注释：
+    // for (Node* p = X.value.head; p != NULL; p = p->next) {
     //     print_solution(p->sol);
     // }
 
-    free_list(&ans);
+    CILK_C_UNREGISTER_REDUCER(X);
     return 0;
 }
